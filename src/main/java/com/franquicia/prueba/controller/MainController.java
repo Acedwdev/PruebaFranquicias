d733713e630695c490793d1dcaa1d7b48468b911
Acedwdev <acedwdev@gmail.com>
2025-04-12 19:54:35 -0500

    Se agrega endpoint para eliminar producto en el controlador

--- src/main/java/com/franquicia/prueba/controller/MainController.java
+++ src/main/java/com/franquicia/prueba/controller/MainController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -81,6 +82,11 @@ public class MainController {
             prod.setStock(p.getStock());
             return DtoMapper.toProductoDTO(productoService.save(prod));
         }).orElseThrow();
+    }
+    
+    @DeleteMapping("/productos/{id}")
+    public void eliminarProducto(@PathVariable Long id) {
+        productoService.deleteById(id);
     }
     
     @GetMapping("/franquicias/{franquiciaId}/productos/max-stock")
